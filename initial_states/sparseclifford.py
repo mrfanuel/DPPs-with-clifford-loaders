c638f86d925de8133c0a3b4a7666b7a92f02c7eb
Michaël Fanuel <mrfanuel@hotmail.fr>
2025-10-17 14:25:12 +0200

    Z gates in sparse loader (Jordan-Wigner)

--- initial_states/sparseclifford.py
+++ initial_states/sparseclifford.py
@@ -23,7 +23,7 @@ import numpy as np
 from qiskit import QuantumCircuit, QuantumRegister
 from qiskit_nature.second_q.mappers import QubitMapper
 from qiskit_nature.second_q.mappers import JordanWignerMapper
-from qiskit.circuit.library import XGate
+from qiskit.circuit.library import XGate, ZGate
 
 from .utils.givensdecompose import _givens_decompose_column_sparse
 
@@ -122,6 +122,101 @@ class SparseCliffordLoader(QuantumCircuit):
                     self.append(qc.inverse(),register)
                     # top of the pyramid representing (c + c^*)
                     self.append(XGate(), [register[pyramidion]])
+                    # adding the Z gates appearing in Jordan-Wigner representation
+                    for j in range(0,pyramidion):
+                        self.append(ZGate(), [register[j]])
+                    # 
+                    # Append U
+                    self.append(qc,register)
+        else:
+            raise NotImplementedError(
+                "Currently, only the Jordan-Wigner Transform is supported. "
+                "Please use the qiskit_nature.second_q.mappers.JordanWignerMapper."
+            )
+            
+class ErrorSparseCliffordLoader(QuantumCircuit):
+    r"""A circuit that prepares a Clifford loader for loading a sparse vector such as a column of an edge-vertex incidence matrix.
+    Clifford loader is a state of the form
+
+    .. math::
+        \mathcal{C}(x_1) \dots \mathcal{C}(x_r) \lvert \text{vac} \rangle,
+
+    where
+
+    .. math::
+        \mathcal{C}(x_i) = \mathcal{U}(x_i) (a_1 + a^\dagger_1) \mathcal{U}(x_i)^\dagger.
+    Here .. math::\mathcal{U}(x_i) is implemented with sequencial Givens rotations. The resulting circuit has the shape of a pyramid.
+
+    - :math:`x_i` are :math:`N \times 1` unit norm columns of data matrix.
+    - :math:`a^\dagger_1, \ldots, a^\dagger_{N}` are the fermionic creation operators.
+    - :math:`\lvert \text{vac} \rangle` is the vacuum state.
+      (mutual 0-eigenvector of the fermionic number operators :math:`\{a^\dagger_j a_j\}`)
+
+
+    Currently, only the Jordan-Wigner transformation is supported.
+
+    """
+
+
+    def __init__(
+        self,
+        data_matrix: np.ndarray,
+        qubit_mapper: QubitMapper | None = None,
+        *,
+        validate: bool = True,
+        rtol: float = 1e-5,
+        atol: float = 1e-8,
+        **circuit_kwargs,
+    ) -> None:
+        # pylint: disable=unused-argument
+        r"""
+        Args:
+            data_matrix: matrix whose columns are loaded.
+                The columns of the matrix must be have unit 2-norm.
+            qubit_mapper: The ``QubitMapper`` or ``QubitConverter`` (use of the latter is
+                deprecated). The default behavior is to create one using the call
+                ``JordanWignerMapper()``.
+            qubit_converter: DEPRECATED The ``QubitConverter`` or ``QubitMapper``. The default
+                behavior is to create one using the call ``JordanWignerMapper()``.
+            validate: Whether to validate the inputs.
+            rtol: Relative numerical tolerance for input validation.
+            atol: Absolute numerical tolerance for input validation.
+            circuit_kwargs: Keyword arguments to pass to the ``QuantumCircuit`` initializer.
+
+        Raises:
+            ValueError: data_matrix must be a 2-dimensional array.
+            ValueError: data_matrix must have unit norm columns.
+            NotImplementedError: Currently, only the Jordan-Wigner Transform is supported.
+                Please use the :class:`qiskit_nature.second_q.mappers.JordanWignerMapper`.
+        """
+        if validate:
+            _validate_data_matrix(data_matrix, rtol=rtol, atol=atol)
+
+        if qubit_mapper is None:
+            qubit_mapper = JordanWignerMapper()
+
+        n, m = data_matrix.shape
+        register = QuantumRegister(n)
+        super().__init__(register, **circuit_kwargs)
+
+        if isinstance(qubit_mapper, JordanWignerMapper):
+            
+                for i in range(m-1,-1,-1): # over the columns in reverse order
+                    # decomposition of U                    
+                    decomposition_reversed, pyramidion  = _givens_decompose_column_sparse(register, data_matrix[:,[i]])
+                    # we build U (c + c^*) U^{-1}
+                    # Circuit of U
+                    qc = QuantumCircuit(register)
+                    for gate, qubits in decomposition_reversed:
+                        qc.append(gate, qubits)
+                    # Append U^{-1}
+                    self.append(qc.inverse(),register)
+                    # top of the pyramid representing (c + c^*)
+                    self.append(XGate(), [register[pyramidion]])
+                    # adding Z gates (due to Jordan-Wigner representation of CAR)
+                    for j in range(0,pyramidion):
+                        self.append(ZGate(), [register[j]])
+                    
                     # Append U
                     self.append(qc,register)
         else:
